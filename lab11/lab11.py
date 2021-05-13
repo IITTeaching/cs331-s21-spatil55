53a4cb121081b8191bae4701e817341eb083890b
spatil55 <77546894+spatil55@users.noreply.github.com>
2021-05-12 22:57:06 -0500

    Update lab11.py

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -1,3 +1,4 @@
+
 from unittest import TestCase
 import random
 
@@ -6,18 +7,51 @@ def quicksort(lst,pivot_fn):
 
 def qsort(lst,low,high,pivot_fn):
     ### BEGIN SOLUTION
+    if low<high:
+      func = pivot_fn(lst, low, high)
+      turner = lst[func]
+      h = high+1
+      l = low-1
+      while l<h:
+        h=h-1
+        while h>0 and lst[h]>turner:
+          h=h-1
+        l=l+1
+        
+        while l<high and lst[l]<turner:
+          l=l+1
+        
+        if l<h:
+          temp = lst[l]
+          lst[l] = lst[h]
+          lst[h] = temp
+         
+      qsort(lst, low, h, pivot_fn)
+      qsort(lst, h+1, high, pivot_fn)
+        
+
     ### END SOLUTION
 
 def pivot_first(lst,low,high):
     ### BEGIN SOLUTION
+    return low
     ### END SOLUTION
 
 def pivot_random(lst,low,high):
     ### BEGIN SOLUTION
+    return random.randint(low, high)
     ### END SOLUTION
 
 def pivot_median_of_three(lst,low,high):
     ### BEGIN SOLUTION
+    mean = (low+high)//2
+    quicksort([lst[low], lst[mean], lst[high]], pivot_first)
+    if [lst[low], lst[mean], lst[high]][1]==lst[low]:
+      return low
+    if [lst[low], lst[mean], lst[high]][1]==lst[mean]:
+      return mean
+    if [lst[low], lst[mean], lst[high]][1]==lst[high]:
+      return high
     ### END SOLUTION
 
 ################################################################################
